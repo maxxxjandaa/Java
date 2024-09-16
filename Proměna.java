e505951ac3666a034ab7c9407a25ae61122f9a89
unknown <maxmilian.janda@infis.cz>
2024-09-16 13:05:15 +0200

    FileUpdate

--- Proměna.java
+++ Proměna.java
@@ -3,15 +3,14 @@ public class Proměna {
 
         //numeric values
         {
-            byte a = 1; //-128 - 127
-            short b = 1; //-32 768 - 32 767
+
             int c = 1;  // hodně
-            long d = 1; // hodně moc
+
 
         }
         //numeric values with a decimal point
         {
-            float e = 1.0f; //23 b
+
             double f = 1.0d; //52 b
         }
 
